6a4cb714a1a180e7ef375e0bc999db47068057ad
Michael Liang <michaelpliang@gmail.com>
2016-03-28 15:49:11 -0700

    deploy commit to gh-pages

---
+++ app/home/about.route.js
@@ -0,0 +1,24 @@
+'use strict';
+
+(function () {
+    'use strict';
+
+    angular.module('app.home').run(appRun);
+
+    appRun.$inject = ['routerHelper'];
+    /* @ngInject */
+    function appRun(routerHelper) {
+        routerHelper.configureStates(getStates());
+    }
+
+    function getStates() {
+        return [{
+            state: 'home',
+            config: {
+                url: '/',
+                templateUrl: 'app/home/home.html',
+                title: 'home'
+            }
+        }];
+    }
+})();

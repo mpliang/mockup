f97cf67c4c27dc999dab395f9578764e0951fdbd
Michael Liang <michaelpliang@gmail.com>
2016-03-28 17:20:01 -0700

    server update

--- src/server/app.js
+++ src/server/app.js
@@ -10,9 +10,9 @@ var cors = require('cors');
 var port = process.env.PORT || 8001;
 var four0four = require('./utils/404')();
 
-var environment = process.env.NODE_ENV;
+var environment = 'build';
 
-var whitelist = ['https://mpliang.github.io'];
+var whitelist = ['http://localhost:3000'];
 var corsOptions = {
   origin: function(origin, callback) {
     var originIsWhitelisted = whitelist.indexOf(origin) !== -1;
@@ -35,13 +35,13 @@ console.log('NODE_ENV=' + environment);
 switch (environment){
     case 'build':
         console.log('** BUILD **');
-        app.use(express.static('./build/'));
+        app.use(express.static('./dist/'));
         // Any invalid calls for templateUrls are under app/* and should return 404
         app.use('/app/*', function(req, res, next) {
             four0four.send404(req, res);
         });
         // Any deep link calls should return index.html
-        app.use('/*', express.static('./build/index.html'));
+        app.use('/*', express.static('./dist/index.html'));
         break;
     default:
         console.log('** DEV **');
